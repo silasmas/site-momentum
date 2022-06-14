b1a5a1534f4d9142ea767ab6412e7556e731de09
silasmas <silasjmas@gmail.com>
2022-06-14 03:47:34 +0200

    ok

---
+++ resources/views/site/parties/footer.blade.php
@@ -0,0 +1,98 @@
+  <!-- Footer Start -->
+  <footer id="rs-footer" class="rs-footer style1">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-12 col-sm-12 md-mb-10">
+                    <div class="footer-logo mb-40">
+                        <a href="index.html"><img src="assets/images/logo.png" alt=""></a>
+                    </div>
+                      <div class="textwidget white-color pb-40"><p>We denounce with righteous indig nation in and dislike men who are so beguiled and to demo realized by the, so blinded by desire, that they cannot foresee.</p>
+                      </div>
+                      <ul class="footer-social md-mb-30">  
+                          <li> 
+                              <a href="#" target="_blank"><span><i class="fa fa-facebook"></i></span></a> 
+                          </li>
+                          <li> 
+                              <a href="# " target="_blank"><span><i class="fa fa-twitter"></i></span></a> 
+                          </li>
+
+                          <li> 
+                              <a href="# " target="_blank"><span><i class="fa fa-pinterest-p"></i></span></a> 
+                          </li>
+                          <li> 
+                              <a href="# " target="_blank"><span><i class="fa fa-instagram"></i></span></a> 
+                          </li>
+                                                                   
+                      </ul>
+                </div>
+                <div class="col-lg-3 col-md-12 col-sm-12 md-mb-10 pl-55 md-pl-15">
+                    <h3 class="footer-title">Our Services</h3>
+                    <ul class="site-map">
+                        <li><a href="business-planning.html">Business planning</a></li>
+                        <li><a href="tax-strategy.html">Tax strategy</a></li>
+                        <li><a href="financial-advices.html">Financial advices</a></li>
+                        <li><a href="insurance-strategy.html">Insurance strategy</a></li>
+                        <li><a href="manage-investment.html">Manage investment</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-12 col-sm-12 md-mb-10">
+                    <h3 class="footer-title">Contact Info</h3>
+                    <ul class="address-widget">
+                        <li>
+                            <i class="flaticon-location"></i>
+                            <div class="desc">Ta-134/A, Gulshan Badda<br>
+                             Link Rd, Dhaka</div>
+                        </li>
+                        <li>
+                            <i class="flaticon-call"></i>
+                            <div class="desc">
+                               <a href="tel:(+880)15569569365">(+880)155 69569 365</a>
+                            </div>
+                        </li>
+                        <li>
+                            <i class="flaticon-email"></i>
+                            <div class="desc">
+                                <a href="mailto:support@rstheme.com">support@rstheme.com</a>
+                            </div>
+                        </li>
+                        <li>
+                            <i class="flaticon-clock-1"></i>
+                            <div class="desc">
+                                Office Hours: 8AM - 11PM  
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-12 col-sm-12">
+                    <h3 class="footer-title">Newsletter</h3>
+                    <p class="widget-desc white-color">Stay up to update with our latest news and products.</p>
+                    <p>
+                        <input type="email" name="EMAIL" placeholder="Your email address" required="">
+                        <input type="submit" value="Subscribe Now">
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">                    
+            <div class="row y-middle">
+                <div class="col-lg-6 md-mb-10 text-lg-end text-center order-last">
+                    <ul class="copy-right-menu">
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="faqs.html">Faqs</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-6">
+                    <div class="copyright text-lg-start text-center ">
+                        <p>© 2021 Bizup - Consulting Company Inc. All Rights Reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer End -->
